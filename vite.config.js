66d841b23af9216ce4d82bf7b5f8dff04ab11643
Garleans <garleans@gmail.com>
2025-08-04 17:49:07 -0400

    levantando en git pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Bootcamp_Proyecto_5.github.io/',
   plugins: [react()],
 });

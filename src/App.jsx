49a4dce09f4f9a59f9f6d9e7063111d6a2ef6566
Garleans <garleans@gmail.com>
2025-08-04 21:13:13 -0400

    github pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { CssBaseline, Box } from "@mui/material";
 import AppRoutes from "./routes/AppRoutes";
 
+const isGitHubPages = window.location.hostname.includes("github.io");
+
 function App() {
   return (
-    <Router>
+    <Router basename={isGitHubPages ? "/Bootcamp_Proyecto_5" : "/"}>
       <CssBaseline />
       <Box sx={{ minHeight: "100vh" }}>
         <AppRoutes />

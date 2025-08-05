0942a72d9f6ec53d2ab935e6a5d29c0aa03d77b7
Garleans <garleans@gmail.com>
2025-08-04 21:07:57 -0400

    github pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/Bootcamp_Proyecto_5/" : "/", // SOLO usa base en producción
   plugins: [react()],
 });

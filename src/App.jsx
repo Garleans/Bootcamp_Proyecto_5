2a7c0a009ac2b43513d417b6d1bc712dcc6a0baa
Garleans <garleans@gmail.com>
2025-08-09 11:36:51 -0400

    version final 2025-08-09

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { HashRouter as Router } from "react-router-dom";
 import { CssBaseline, Box } from "@mui/material";
 import AppRoutes from "./routes/AppRoutes";
 
-const basename = import.meta.env.MODE === "production" ? "/Bootcamp_Proyecto_5" : "";
-
 function App() {
   return (
     <Router>
